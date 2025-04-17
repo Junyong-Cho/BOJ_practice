271dc11b9bd7ddb308fa1c2342870debef45a157
ChoJunyong <junyong4510@gmail.com>
2025-04-17 20:31:27 +0900

    solving

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,9 +1,26 @@
 import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
 public class Main {
 	
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int n, l, left, right;
+	static int window[];
+	
 	public static void main(String args[]) throws IOException{
-		
+		n = nex();
+		l = nex();
+		window = new int[n];
+		left = right = 0;
+		window[0] = nex();
+		bw.write(window[0]+" ");
+		for(int i = 1; i < l; i++) {
+			int t = nex();
+			if(window[right]>t) {
+				
+			}
+		}
 	}
 	
 	static int nex() throws IOException{
