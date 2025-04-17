23cf9d1d01a64d43fe83125c62b5cf62922d8ca0
ChoJunyong <junyong4510@gmail.com>
2025-04-17 18:35:58 +0900

    fast int IO

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,6 +1,22 @@
+import java.io.IOException;
 
 public class Main {
-	public static void main(String args[]){
-		System.out.println("hello world!");
+	
+	public static void main(String args[]) throws IOException{
+		
+	}
+	
+	static int nex() throws IOException{
+		int n, c;
+		boolean pos = true;
+		while((n = System.in.read())<=' ');
+		if(n=='-') {
+			pos = false;
+			n = System.in.read();
+		}
+		n &= 0b1111;
+		while((c = System.in.read())>='0')
+			n = (n<<3) + (n<<1) + (c&0b1111);
+		return pos?n:~n+1;
 	}
 }
