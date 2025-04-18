7600e040d8980664372f85b138feab02eff75f9d
ChoJunyong <junyong4510@gmail.com>
2025-04-18 12:12:36 +0900

    temp

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -5,7 +5,7 @@ import java.io.OutputStreamWriter;
 public class Main {
 	
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static int n, l, left, right;
+	static int n, l, left, right, t;
 	static int window[][];
 	
 	public static void main(String args[]) throws IOException{
@@ -14,15 +14,60 @@ public class Main {
 		window = new int[n][2];
 		window[0][0] = nex();
 		bw.write(window[0][0]+" ");
-		for(int i = 1; i < n; i++) {
-			int t = nex();
+		for(int i = 1; i < l; i++) {
+			t = nex();
 			if(window[left][0]>t) {
 				left = right = i;
 				window[left][0] = t;
 				window[left][1] = i;
 			}
-			else if(window[right][0]>t);
+			else if(window[right][0]<t) {
+				window[++right][0] = t;
+				window[right][1] = i;
+			}
+			else
+				binarySearch(i);
 			bw.write(window[left][0]+" ");
+		}
+		for(int i = l; i < n; i++) {
+			t = nex();
+			if(window[left][0]>t) {
+				left = right = i;
+				window[left][0] = t;
+				window[left][1] = i;
+			}
+			else if(window[right][0]<t) {
+				window[++right][0] = t;
+				window[right][1] = i;
+			}
+			else
+				binarySearch(i);
+			while(window[left][1]<i-l+1) left++;
+			bw.write(window[left][0]+" ");
+		}
+		
+		bw.flush();
+	}
+	
+	static void binarySearch(int index) {
+		int l = left, r = right;
+		while(true) {
+			int mid = (l+r)>>1;
+			if(window[mid][0]==t) {
+				window[mid][1] = index;
+				right = mid;
+				return;
+			}
+			if(t<window[mid][0])
+				r = mid;
+			else if(t<window[mid+1][0]) {
+				right = mid+1;
+				window[right][0] = t;
+				window[right][1] = index;
+				return;
+			}
+			else
+				l = mid;
 		}
 	}
 	
