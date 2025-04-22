8638f5c419d98ca68175638296dbeea4632f9c73
ChoJunyong <junyong4510@gmail.com>
2025-04-22 11:59:32 +0900

    resetting

---
+++ Main/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class Main {
+	
+	static public void main(String catchsunpie[]) throws IOException{
+		
+	}
+	
+	static int nex() throws IOException{
+		int n, c;
+		while((n = System.in.read())<=' ');
+		n &= 0b1111;
+		while((c = System.in.read())>='0')
+			n = (n<<3) + (n<<1) + (c&0b1111);
+		return n;
+	}
+}
